4f58a5cc166211815f9d815373ecc823f4f8d66e
administrator-at-520044189785 <enzziom@gmail.com>
2021-04-03 22:53:52 -0300

    update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "ubuntu-18" {
+data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
@@ -6,11 +6,11 @@ data "aws_ami" "ubuntu-18" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server*"]
   }
 
-  owners = ["${owners_ami}"] # ID conta AWS Ubuntu
+  owners = [var.owners_ami] # ID conta AWS Ubuntu
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu-18.id
+  ami           = data.aws_ami.ubuntu.id
   for_each      = toset(var.instance_type)
   instance_type = each.value
 
