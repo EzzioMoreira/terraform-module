6e8ee0600833489cfd390b480dafe9a99a0b4184
administrator-at-520044189785 <enzziom@gmail.com>
2021-04-03 22:58:32 -0300

    update variable

--- variable.tf
+++ variable.tf
@@ -1,29 +1,29 @@
+# Id owner conta aws
 variable "owners_ami" {
   description = "Id owner conta aws."
   type = number
   default = 099720109477
 }
-
+# Variavel staging
 variable "environment" {
   default = "production"
 }
 
-variable "plus" {
-  default = 1
-}
-
+# Variavel tipo e quantidade de instância
 variable "instance_type" {
   type        = list(string)
   default     = ["t2.micro","t3.medium"]
   description = "The list of instance type."  
 }
 
+# Variavel TAGS
 variable "tag_name" {
   type = list(number)
   default = [1,2]
   
 }
 
+# Variavel EBS
 variable "blocks" {
   type = list(object(
     {
@@ -31,4 +31,10 @@ variable "blocks" {
       volume_size = string
       volume_type = string
     }))
+}
+
+# Variavel nome do produto
+variable "name" {
+  type        = string
+  description = "Name of product"
 }
